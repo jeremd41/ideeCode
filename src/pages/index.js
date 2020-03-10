08bd399bcd4bbe0f9901914bf8dd2531840b063f
brandon-diard <brandon.diard@gmail.com>
2020-03-10 17:22:57 +0100

    end day 10/03/2020

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import SEO from "../components/seo"
 import Layouti from "../components/layouti"
-import HeroVid from "../components/hero-vid"
+import HeroVidButton from "../components/herovidbutton"
 import { Link } from "gatsby"
 
 import "../components/reset.css"
@@ -13,8 +13,7 @@ class index extends Component {
     return (
         <Layouti bgnav="accueil" btnnav="btn-grey" bgfooter1="accueil1" bgfooter2="accueil2">
           <SEO title="Accueil" />
-          <HeroVid></HeroVid>
-          <Link to="/page-2/" className="link">Page 2</Link>
+          <HeroVidButton></HeroVidButton>
         </Layouti>
     )
   }
