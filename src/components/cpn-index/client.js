19a225aba17df6eedb4637719f17e6ee40d8485e
jeremy41 <jeremy.diard37@gmail.com>
2020-05-03 23:31:00 +0200

    acceuil refacto ok

--- src/components/cpn-index/client.js
+++ src/components/cpn-index/client.js
@@ -4,19 +4,21 @@ import Client1 from "../../images/client1.png"
 import Client2 from "../../images/client2.png"
 import Client3 from "../../images/client3.png"
 import Client4 from "../../images/client4.svg"
+import Client5 from "../../images/client5.png"
 
 class Client extends Component {
     render() {
         return (
             <section className="client-container">
 
-                <h2>Ils m'ont fait confiance !</h2>
+                <h2>Nos clients et partenaires </h2>
 
                 <div className="client">
-                    <img src={Client1}/>
-                    <img src={Client2}/>
-                    <img src={Client3}/>
-                    <img src={Client4}/>
+                    <img src={Client1} alt="logo-client"/>
+                    <img src={Client2} alt="logo-client"/>
+                    <img src={Client3} alt="logo-client"/>
+                    <img src={Client4} alt="logo-client"/>
+                    <img src={Client5} alt="logo-client"/>
                 </div>
 
             </section>
--- src/pages/solutions.js
+++ src/pages/solutions.js
@@ -3,6 +3,7 @@ import SEO from "../components/seo"
 import Layouti from '../components/layouti'
 import HeroVidTitle from '../components/herovidtitle'
 import ContentSolutions from '../components/cpn-solutions/contentsolutions'
+import Client from "../components/cpn-index/client"
 import Emailing from '../components/emailing'
 
 import solutionsvid from '../images/solutions.mp4'
@@ -13,6 +14,7 @@ class Solutions extends Component {
             <Layouti bgnav="accueil" btnnav="btn-grey" bgfooter1="accueil1" bgfooter2="accueil2">
                 <SEO title="Solutions" />
                 <HeroVidTitle videobg={solutionsvid} title="Solutions"/>
+                <Client />
                 <ContentSolutions/>
                 <Emailing/>
             </Layouti>
