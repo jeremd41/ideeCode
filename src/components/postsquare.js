8dda4727c463c97cc32f0412d557842a8d639764
Jeremy <jeremy.diard37@gmail.com>
2020-09-22 22:27:05 +0200

    responsive acceuil ok

--- src/components/postsquare.js
+++ src/components/postsquare.js
@@ -7,15 +7,15 @@ class PostSquare extends Component {
     render () {
         return (
             <section className="postsquare">
-                <div className="img-container">
+                <Link to={this.props.slug} className="img-container">
                     <Img fluid={this.props.imgpostsquare} alt={this.props.altpostsquare}/>
-                </div>
+                </Link>
                 <div className="half-flex">
                     <div className="content-container">
                         <Link to={this.props.slug}>
                             <h3>{this.props.title}</h3>
                         </Link>
-                        <p>{this.props.excerpt.substring(0, 100)}...</p>
+                        <p>{this.props.excerpt.substring(0, 60)}...</p>
                     </div>
                     <div className="categories-container">
                         {this.props.tags.map((item, key) => {
